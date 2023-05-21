6ae7865aaededcc875f17909d43eaf906fce12f6
yasharzavary <zavaryyashar009@gmail.com>
2023-05-21 11:52:11 +0430

    our decode encode menu finish...now i gonna to coding part

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
+import re
 
 # main gui part
 root=Tk()
@@ -29,7 +30,7 @@ coderFrame.pack(side='bottom')
 coderFrame.pack_propagate(0)
 
 # programmer stamp
-stampLabel=Label(master=coderFrame, text='@zavaryayshar2023', bg='#707070')
+stampLabel=Label(master=coderFrame, text='@Copyright-zavaryayshar2023', bg='#707070')
 stampLabel.pack(side='right')
 
 # url label place parts
@@ -59,7 +60,82 @@ fileButton.pack()
 # progress start Button
 # my main func for coding the picture
 def progress(event):
-    print(filename)
+    # my name part root 
+    nameRoot=Tk()
+    nw=500
+    nh=250
+    nx=(screenw/2 - nw/2)
+    ny=(screenh/2 - nh/2)
+    nameRoot.geometry('%dx%d+%d+%d'%(nw,nh,nx,ny))
+    nameRoot.title('name select')
+    nameRoot.iconbitmap('nameroot.ico')
+        
+    writeLabel=Label(master=nameRoot, text=f'write your sentence to code on the picture:\n{filename}')
+    writeLabel.pack()
+        
+    nameEntry=Entry(master=nameRoot)
+    nameEntry.pack()
+        
+    # my coding part
+    def startProgress(event):
+        pass
+        
+    def comein(event):
+        okButton.config(bg='gray')
+            
+    def comeout(event):
+        okButton.config(bg='#f8f8f9')
+        
+    okButton=Button(master=nameRoot, text='start progress')
+    okButton.bind('<Enter>',comein)
+    okButton.bind('<Leave>',comeout)
+    okButton.bind('<Button>',comeout)
+    okButton.pack()
+        
+    nameRoot.mainloop()
+
+def dprogress(event):
+    pass
+
+
+onetimeclick=True
+def select(event):
+    global onetimeclick
+    if filename=='':
+        messagebox.showerror('picture Error', 'you don\'t select a picture')
+        return
+    elif re.search(r'.+jpg', filename):
+        if onetimeclick:
+            onetimeclick=False
+            
+            def comein(event):
+                encodeButton.config(bg='red')
+
+            def comeout(event):
+                encodeButton.config(bg='#f8f8f9')
+            
+            encodeButton=Button(master=root, text='encode', bg='#f8f8f9')
+            encodeButton.bind('<Button>',progress)
+            encodeButton.bind('<Enter>',comein)
+            encodeButton.bind('<Leave>',comeout)
+            encodeButton.pack(side='right')
+            
+            def comein(event):
+                decodeButton.config(bg='green')
+
+            def comeout(event):
+                decodeButton.config(bg='#f8f8f9')
+            
+            decodeButton=Button(master=root, text='decode')
+            decodeButton.bind('<Button>',dprogress)
+            decodeButton.bind('<Enter>',comein)
+            decodeButton.bind('<Leave>',comeout)
+            decodeButton.pack(side='left')
+        else:
+            return
+    else:
+        messagebox.showerror('picture Error', 'this url is isn\'t a picture')
+        return
 
 def comein(event):
     startButton.config(bg='gray')
@@ -67,8 +143,8 @@ def comein(event):
 def comeout(event):
     startButton.config(bg='#f8f8f9')
     
-startButton=Button(master=root,text='Progress',bg='#f8f8f9')
-startButton.bind('<Button>',progress)
+startButton=Button(master=root,text='select',bg='#f8f8f9')
+startButton.bind('<Button>',select)
 startButton.bind('<Enter>',comein)
 startButton.bind('<Leave>',comeout)
 startButton.pack()
